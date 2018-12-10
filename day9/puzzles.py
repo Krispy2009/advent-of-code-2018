5ea806d7023f835e32d1292cead08f2f420ce361
Kristi Achilleos <krispy2009@gmail.com>
2018-12-10 08:18:18 +0200

    Day 9 part 2 done

--- day9/puzzles.py
+++ day9/puzzles.py
@@ -1,3 +1,4 @@
+from blist import blist
 
 
 def pretty_print(circle, idx):
@@ -6,37 +7,42 @@ def pretty_print(circle, idx):
     return pretty
 
 
-def play(num_players, marbles):
-
-    marbles = list(range(marbles+1))
+def play(num_players, num_marbles):
     current_marble_idx = 0
     players = [0] * num_players
-    circle = [marbles.pop(0)]
-    # print(f'[-]', pretty_print(circle, current_marble_idx))
+    circle = blist([0])
+    print(f'[-]', pretty_print(circle, current_marble_idx))
+    current_marble = 1
+    while True:
+        player = 0
+        while player < num_players:
 
-    # import pudb; pu.db
-    while marbles:
-        for player in range(num_players):
-            if marbles:
-                current_marble = marbles.pop(0)
-                # current_marble_idx = 1
-                if current_marble % 23 == 0:
-                    players[player] += current_marble
-                    players[player] += circle.pop(current_marble_idx - 6)
-                    current_marble_idx = current_marble_idx - 7
-                    current_marble = circle[current_marble_idx]
-                else:
-                    if current_marble == 1:
-                        circle.append(current_marble)
-                        current_marble_idx = 1
-                    else:
-                        insert_idx = ((current_marble_idx+2) % (len(circle)))
-                        circle.insert(insert_idx+1, current_marble)
-                        current_marble_idx = insert_idx
-                # print(f'[{player+1}]',pretty_print(circle, current_marble_idx))
+            if current_marble % 23 == 0:
+                players[player] += current_marble
+                current_marble_idx = current_marble_idx - 7
+                if current_marble_idx < 0:
+                    current_marble_idx += len(circle)
+                players[player] += circle.pop(current_marble_idx)
+
             else:
-                print(max(players))
-                break
+                if current_marble == 1:
+                    circle.append(current_marble)
+                    current_marble_idx = 1
+                else:
+                    insert_idx = ((current_marble_idx+2) % (len(circle)))
+                    if insert_idx == 0:
+                        insert_idx = len(circle)
+                    circle.insert(insert_idx, current_marble)
+                    current_marble_idx = insert_idx
+            current_marble += 1
+
+            player += 1
+            #print(f'[{player}]', pretty_print(circle, current_marble_idx))
+
+            if current_marble > num_marbles:
+                print(f'{num_players} - {num_marbles} --> {max(players)}')
+                return
+
 
 if __name__ == '__main__':
     """
@@ -50,12 +56,22 @@ if __name__ == '__main__':
 
     challenge:
     464 players; last marble is worth 71730 points
+
+    extra examples:
+    9 players; last marble is worth 48 points: high score is 63
+    1 players; last marble is worth 48 points: high score is 95
+
     """
 
-    # play(9, 25)
-    # play(10, 1618)
-    # play(13, 7999)
-    # play(17, 1104)
-    # play(21, 6111)
-    # play(30, 5807)
-    play(464, 71730)
+
+    #play(9, 25) # 32
+    #play(10, 1618) # 8317
+    # play(13, 7999) # 146373
+    # play(17, 1104) # 2764
+    #play(21, 6111) # 54718
+    # play(30, 5807) # 37305
+
+    #Part 1
+    # play(464, 71730) # 380705
+    #Part 2
+    play(464, 7173000) # 3175950420
